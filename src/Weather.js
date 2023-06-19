9b38595d2f206d0f9c311da6dc4ad90e95a4cb5d
anna <anutka2610@gmail.com>
2023-06-19 23:33:38 +0200

    made a form

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,22 @@
 import React from "react";
 
 export default function Weather() {
-  return <div>Hello from Weather</div>;
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city..."
+              className="form-contol"
+            />
+          </div>
+          <div className="col-3">
+            <input type="submit" value="search" className="btn btn-primary" />
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 }
