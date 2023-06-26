93ff2c05d91f2ab452f2e4062bb8ad7d5bd259c6
anna <anutka2610@gmail.com>
2023-06-26 17:21:58 +0200

    created a layout of forecast column

---
+++ src/WeatherForecast.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import "./WeatherForecast.css";
+
+export default function WeatherForecast() {
+  return (
+    <div className="WeatherForecast">
+      <div>Thu</div>
+      "icon"
+      <div>
+        <span className="WeatherForecast-max">max</span>/
+        <span className="WeatherForecast-min">min</span>
+      </div>
+    </div>
+  );
+}
