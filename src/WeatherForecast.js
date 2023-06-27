aad0826cc7fdb9f788d4d7cd5fa6003d297f99ff
anna <anutka2610@gmail.com>
2023-06-27 23:05:05 +0200

    made a forecast for 5 days.

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -16,7 +16,17 @@ export default function WeatherForecast(props) {
   if (load) {
     return (
       <div className="WeatherForecast">
-        <WeatherForecastDay data={forecast[0]} />
+        <div className="row">
+          {forecast.map(function (dailyForecast, index) {
+            if (index < 5) {
+              return (
+                <div className="col" key={index}>
+                  <WeatherForecastDay data={dailyForecast} />
+                </div>
+              );
+            }
+          })}
+        </div>
       </div>
     );
   } else {
